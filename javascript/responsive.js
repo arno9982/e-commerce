a6931bc5c9743577d17c61bc0222216bd821ebcb
DjouArthur <arthurdjou9@gmail.com>
2025-12-04 14:36:31 +0100

    responsivite page Home updated

--- javascript/responsive.js
+++ javascript/responsive.js
@@ -187,7 +187,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         }, 200);
     });
-
+   
     // === LANCEMENT ===
     createMobileMenu();
     syncLanguageAndCurrency();
